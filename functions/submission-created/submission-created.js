70dfa5321d51ccded09d0750a019879680cd5e68
m0rth1um <morth@kmpt.nz>
2021-02-24 12:24:09 +0100

    Background

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,20 +1,25 @@
-const process = require('process')
 const TelegramBot = require('telegrambot');
 
-const { TG_BOT_TOKEN, TG_CHAT_ID } = process.env
-
 const handler = async (event) => {
-  if (TG_BOT_TOKEN === undefined || TG_CHAT_ID === undefined) {
+  console.log(event)
+
+  if (process.env.TG_BOT_TOKEN === undefined || process.env.TG_CHAT_ID === undefined) {
+    console.log( 'Env vars must be provided!');
+
     return { statusCode: 422, body: 'Env vars must be provided!' }
   }
 
-  const api = new TelegramBot(TG_BOT_TOKEN);
+  const api = new TelegramBot(process.env.TG_BOT_TOKEN);
 
   const payload = JSON.stringify(event)
 
   try {
-    api.sendMessage({ chat_id: TG_CHAT_ID, text: payload }, function (err, message) {
+    api.sendMessage({
+      chat_id: process.env.TG_CHAT_ID,
+      text: payload
+    }, function (err, message) {
       if (err) throw err;
+      console.log(String(err))
     });
   } catch (error) {
     return { statusCode: 422, body: String(error) }
